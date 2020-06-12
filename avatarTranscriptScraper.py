dc3f6cad929bdf63ee276c91c0cb80f646820cc1
Brandon Fannin <bfannin13@gmail.com>
2020-06-12 14:14:13 -0400

    refactor web scraper, save results to json file

--- avatarTranscriptScraper.py
+++ avatarTranscriptScraper.py
@@ -4,27 +4,37 @@ import urllib.request
 import re
 # import web scraping library
 from bs4 import BeautifulSoup
+# import json handling library
+import json
 
-# get the html content of a url.
 def html_from_url(url):
+  """
+  get the html content of a url.
+  """
   fp = urllib.request.urlopen(url)
   mybytes = fp.read()
   fp.close()
   return mybytes.decode("utf8")
 
-# make a full url to the transcript from a links href tag
 def make_link(aTag):
+  """
+  make a full url to the transcript from a links href tag
+  """
   return "https://avatar.fandom.com" + aTag['href']
 
-# get beautiful soup from the avatar fan wiki
 def get_avatar_fan_wiki_soup():
-    wikiURL = "https://avatar.fandom.com/wiki/Avatar_Wiki:Transcripts"
-    transcriptSoup = BeautifulSoup(html_from_url(wikiURL), "html.parser")
-    return transcriptSoup
+  """
+  get beautiful soup from the avatar fan wiki
+  """
+  wikiURL = "https://avatar.fandom.com/wiki/Avatar_Wiki:Transcripts"
+  transcriptSoup = BeautifulSoup(html_from_url(wikiURL), "html.parser")
+  return transcriptSoup
 
-# check if a link is a transcript link
-# an if it comes from Avatar Book One-Three
 def is_transcript_link(tag):
+  """
+  check if a link is a transcript link
+  and if it comes from Avatar Book One-Three
+  """
   isLink = tag.has_attr('href') and tag.has_attr('title')
   if not isLink:
     return False
@@ -44,8 +54,10 @@ def is_transcript_link(tag):
   result = re.compile('Book_One:_Water|Book_Two:_Earth|Book_Three:_Fire').search(headerId)
   return result
 
-# get transcript links from a beautiful soup object
 def get_transcript_links(transcriptSoup):
+  """
+  get transcript links from a beautiful soup object
+  """
   transcriptLinkElements = transcriptSoup.find_all(is_transcript_link)
   transcriptLinks = []
 
@@ -53,28 +65,50 @@ def get_transcript_links(transcriptSoup):
     transcriptLinks.append(make_link(elem))
   return transcriptLinks
 
-# define an episode class
 class Episode:
+  """
+  define an episode class
+  """
   def __init__(self, title, lines):
     self.title = title
     self.lines = lines
 
-# convert an episode's link to a python object
-# with title and lines of the episode
 def link_to_episode(link):
-    soup = BeautifulSoup(html_from_url(link), "html.parser")
-    title = soup.select('.page-header__title')[0].text[11:]
-    lines = []
-    for tr in soup.select('.wikitable > tr'):
-        if (tr.th):
-            lines.append([tr.th.text.strip(), tr.td.text.strip()])
-    return Episode(title, lines)
+  """
+  convert an episode's link to a python object
+  with title and lines of the episode
+  """
+  soup = BeautifulSoup(html_from_url(link), "html.parser")
+  title = soup.select('.page-header__title')[0].text[11:]
+  lines = []
+  for tr in soup.select('.wikitable > tr'):
+      if (tr.th):
+          lines.append([tr.th.text.strip(), tr.td.text.strip()])
+  return Episode(title, lines)
 
-# main
+def save_avatar_episodes_as_json(episodes):
+  """
+  open a json file and write the avatar episodes
+  """
+  eps = {
+    'episodes': [{"title": e.title, "lines": e.lines} for e in episodes]
+  }
 
-# get the html doc to main wiki
-avatarTranscriptSoup = get_avatar_fan_wiki_soup()
-# get all the avatar transcript links
-transcriptLinks = get_transcript_links(avatarTranscriptSoup)
-# convert all the links to episode objects
-avatarEpisodes = [link_to_episode(link) for link in transcriptLinks]
+  with open('avatar-episodes.json', 'w') as f:
+    json.dump(eps, f)
+
+def main():
+  """
+  scrape the avatar fan wiki for episode links
+  save avatar episodes as json file
+  """
+  # get the html doc to main wiki
+  avatarTranscriptSoup = get_avatar_fan_wiki_soup()
+  # get all the avatar transcript links
+  transcriptLinks = get_transcript_links(avatarTranscriptSoup)
+  # convert all the links to episode objects
+  avatarEpisodes = [link_to_episode(link) for link in transcriptLinks]
+  # save episodes as a json file
+  save_avatar_episodes_as_json(avatarEpisodes)
+
+main()
